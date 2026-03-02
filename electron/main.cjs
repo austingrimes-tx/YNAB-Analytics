57c3169e863fd53acb7f882e870cf342753d7650
YNAB Analytics User <ynab-analytics@local>
2026-03-02 16:17:34 -0600

    Add Electron desktop packaging support

---
+++ electron/main.cjs
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = !app.isPackaged;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+        title: "YNAB Analytics",
+        icon: path.join(__dirname, '../public/vite.svg') // Placeholder icon
+    });
+
+    if (isDev) {
+        win.loadURL('http://localhost:5173');
+        win.webContents.openDevTools();
+    } else {
+        win.loadFile(path.join(__dirname, '../dist/index.html'));
+    }
+
+    // Remove menu bar in production
+    if (!isDev) {
+        win.setMenuBarVisibility(false);
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
